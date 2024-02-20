5ca67f40352fb6526e6f8080d6ec6183907d6c68
Kojiiya <tommkennett@gmail.com>
2024-02-20 11:21:57 +0000

    honestly not sure

--- Scanner.java
+++ Scanner.java
@@ -80,9 +80,124 @@ public class Scanner {
 		nextCh();
 	}
 
+	private static boolean isAlphanumerical(char c) {
+		return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
+	}
+	
+	private static void readName(Token t){
+		t.val = "";
+		t.val+=ch;
+
+		while (isAlphanumerical(ch)){
+				t.val+=ch;
+				nextCh();
+		}
+
+		String keyword=""; 
+		for (String s : key){
+			if (t.val == s){
+				keyword = s;
+			}
+		}
+		switch (keyword){
+			case "class": t.kind = class_; break;
+			case "else": t.kind = else_; break;
+			case "final": t.kind = final_; break;
+			case "if": t.kind = if_; break;
+			case "new": t.kind = new_; break;
+			case "print": t.kind = print_; break;
+			case "program": t.kind = program_;
+			case "read": t.kind = read_;
+			case "return": t.kind = return_;
+			case "void": t.kind = void_;
+			case "while": t.kind = while_;
+			default: t.kind = ident; break;
+		}
+	}
+
+	private static void readNumber(Token t){
+		t.val = "";
+
+		boolean flag = true;
+		while (flag){
+			if (ch >= 0 && ch <= 9){
+				t.val += ch;
+			} else {
+				flag = false;
+			}
+		}
+		try {
+			t.numVal = Integer.parseInt(t.val);
+		} catch (Exception e){
+			ch = eofCh;
+		}
+		//t.numVal += (int)(ch);
+	}
 	//---------- Return next input token
 	public static Token next() {
-		TODO // add your code here
+		 // add your code here
+		 while (ch <=' ')nextCh(); //skips blanks, tabs, eol
+		 Token t = new Token(); t.line=line; t.col=col;
+		 switch (ch) {
+			//names, keywords
+			case 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+				 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 
+				 'm', 'n', 'o', 'q', 'r', 's', 't', 'u', 'w', 'x', 'y', 'z',
+				 'M', 'N', 'O', 'Q', 'R', 'S', 'T', 'U', 'W', 'X', 'Y', 'Z':
+				readName(t); 
+				break;
+			//numbers
+			case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+				readNumber(t);
+				break;
+			//simple tokens
+			case ';': nextCh(); t.kind=semicolon; break;
+			case '.': nextCh(); t.kind=period; break;
+			case ',': nextCh(); t.kind=comma; break;
+
+				//operators (divide in comment)
+			case '+': nextCh(); t.kind=plus; break;
+			case '-': nextCh(); t.kind=minus; break;
+			case '*': nextCh(); t.kind=times; break;
+			case '%': nextCh(); t.kind=rem; break;
+
+				//brackets/braces/parentheses
+			case '(': nextCh(); t.kind=lpar; break;
+			case ')': nextCh(); t.kind=rpar; break;
+			case '[': nextCh(); t.kind=lbrack; break;
+			case ']': nextCh(); t.kind=rbrack; break;
+			case '{': nextCh(); t.kind=lbrace; break;
+			case '}': nextCh(); t.kind=rbrace; break;
+
+			case eofCh: t.kind = eof; break; //no nextCh()
+			//compound tokens
+			case '=': 
+				nextCh(); 
+				if (ch =='='){nextCh(); t.kind=eql;} else t.kind = assign;
+				break;
+			case '!':
+				nextCh();
+				if (ch=='='){nextCh(); t.kind=neq;}
+			case '<':
+				nextCh();
+				if (ch=='='){nextCh(); t.kind=leq;} else t.kind = lss;
+			case '>':
+				nextCh();
+				if (ch=='='){nextCh(); t.kind=geq;} else t.kind = gtr;
+				
+			//comments && slash
+			case '/':
+				nextCh();
+				if (ch == '/') {
+					do nextCh(); while (ch != '\n' && ch != eofCh);
+					t = next(); // call scanner recursively
+					} else t.kind = slash;
+				break;
+			default:
+				nextCh(); t.kind=none;
+				break;
+		 }
+		 return t;
 	}
 }
 
